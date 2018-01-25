028ce29ecd83f641461995b0602c01f532afde77
Tom Jacobs <thomas.i.jacobs@accenture.com>
2018-01-25 10:58:31 -0600

    Added home page router

---
+++ src/routes/home.js
@@ -0,0 +1,20 @@
+import logger from '../logger';
+import Conversation from '../lib/watson/conversation';
+
+const conversation = new Conversation();
+
+// Middleware for default route
+const beginConversation = async (req, res) => {
+  let stringifiedOutput: string = '';
+  try {
+    const response: Object = await conversation.message('Hello');
+    const { output: { text } } = response;
+    stringifiedOutput = text.join('. ');
+  } catch (error) {
+    logger.error(error);
+    stringifiedOutput = `There was an error retrieving a response from the conversation service!`;
+  }
+  res.send(stringifiedOutput);
+};
+
+export default beginConversation;
