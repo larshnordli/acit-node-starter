7971718c5d0dd2b6f73c710e844b9e12b99a32af
Brian Leppez <brian.leppez@accenture.com>
2018-01-31 10:03:13 -0600

    Removed unnecessary endpoint

--- src/router.js
+++ src/router.js
@@ -2,12 +2,10 @@
 
 import { Router } from 'express';
 import message from './routes/api/message';
-import beginConversation from './routes/home';
 
 const router = new Router();
 
 // Register your routes and middleware to handle them here!!
 router.post('/api/message', message);
-router.get('/', beginConversation);
 
 export default router;
