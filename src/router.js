0bc94f0f3e415442be37aed8a034d36cb020f9c5
Brian Leppez <brian.leppez@accenture.com>
2018-02-16 20:43:06 -0600

    more housekeeping

--- src/router.js
+++ src/router.js
@@ -6,4 +6,10 @@ const router = new Router();
 
 // Register your endpoints and middleware to handle them here
 
+const defaultEndpoint = (req, res) => {
+  res.send('Your NodeJS server is live');
+};
+
+router.get('/', defaultEndpoint);
+
 export default router;
