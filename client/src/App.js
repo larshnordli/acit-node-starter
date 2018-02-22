b02234e9b762e6ae61842b6f2459b5bc68e58aad
Brian Leppez <brian.leppez@accenture.com>
2018-02-22 13:24:39 -0800

    edited home page

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <Title />
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, clone this repo and code away.
         </p>
       </div>
     );
